bea05aaa77290926cdef4718c0c01515bba16cd3
Eric Burns <ericjosephburns@gmail.com>
2012-10-20 14:13:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LevelBadger::Application.config.secret_token = 'a15bf75f48e20d110ea39533aa8d8a26b4429cee7245cfb3a9f156aabb6943b024288b4f73e15dd4f2f59e2809aeaccdc0c6361a534f971c8f3541b33cc855c1'
