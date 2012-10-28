8f040edd6b3cde29d80bda1c48a3c79ae1e0a7d8
Alex Park <apark@vailsys.com>
2012-10-28 16:32:46 -0500

    added user, level, and friendship data into db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+require 'csv'
+
+Level.destroy_all
+CSV.foreach("db/level_seed.csv") do | row |
+  Level.create!({ xp_to_next_level: row[0] })
+  puts "level with #{row[0]} created"
+end
+
+User.destroy_all
+CSV.foreach("db/user_seed.csv") do | row |
+  level = Level.find_by_xp_to_next_level(row[0])
+  level.users.create!({ username: row[1],
+                        level_id: level.id,
+                        xp: row[2] })
+  puts "#{row[1]}, created!"
+end
+
+Friendship.destroy_all
+CSV.foreach("db/friendship_seed.csv") do | row |
+  user = User.find_by_username(row[0])
+  friend = User.find_by_username(row[1])
+  user.friendships.create!({ user_id: user.id,
+                             friend_id: friend.id})
+  puts "#{user.username} is now friends with #{friend.username}"
+end
