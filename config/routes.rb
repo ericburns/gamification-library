b003d6ce2797a492777140a4e512fa12adb11610
Eric Burns <ericjosephburns@gmail.com>
2012-10-31 21:55:52 -0700

    Added new and update forms for badges, emails, friendships, inventories, levels, and users. Also added friendships to resources so it can be accessed directly.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ LevelBadger::Application.routes.draw do
 
   resources :users
 
+  resources :friendships
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
