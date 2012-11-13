dc0ae0aacf4a63409b3698fa40dbc0bf73a4743e
Cynon <cynon777@gmail.com>
2012-11-13 16:39:11 -0600

    Changed game description to text.

--- db/migrate/20121106230904_create_games.rb
+++ db/migrate/20121106230904_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
     	t.string :gamename, :null => false
-    	t.string :description, :null => false
+    	t.text :description, :null => false
 		t.string :unit, :null => false, :default => 'XP'
 		t.timestamps
     end
