57d1f31dd008993af11723ef3e5fbc4efd3cc34f
Cynon <cynon777@gmail.com>
2012-11-16 03:00:39 -0600

    Working on making levels sort themselves out...

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,7 +1,7 @@
 class Level < ActiveRecord::Base
 	# -------------- Accessors/Mutators
 	attr_accessible :xp_to_next_level, :game_id, :levelno
-	default_scope order('levelno ASC')
+	default_scope order('levelno ASC, xp_to_next_level ASC')
 
 	# -------------- Associations
 	belongs_to :game
@@ -10,6 +10,7 @@ class Level < ActiveRecord::Base
 	after_save :enforce_sequential_levels
 
 	# -------------- Validations
+	before_save :enforce_sequential_levels
 	validates :xp_to_next_level, :presence => true
 	validates :xp_to_next_level, :numericality => {:greater_than => 0}
 	validate :xp_higher_than_previous_level_xp
@@ -19,32 +20,32 @@ class Level < ActiveRecord::Base
 
 	# -------------- Custom Validation Methods
 	def xp_higher_than_previous_level_xp
-		#laster = Level.where("game_id = ?", :game_id)
-		laster = Level.where("game_id = ?", :game_id)
-		laster.order("levelno")
-		last = laster.find(:last)
-		#last = Level.find(:last)
+		game_levels = Level.where("game_id = ?", :game_id)
+        game_levels.order("levelno")
+        last = game_levels.find(:last)
 		if last != nil && last.xp_to_next_level >= xp_to_next_level
 				errors.add(:xp_to_next_level, "is lower or equal to previous level's experience.")
 		end
 	end
 	
 	def assign_levelno
-		gamelevels = Level.find(self.game_id).game.levels
-	    gamelevels.order('levelno')
-		howmanylevels = gamelevels.count
-		levelno = howmanylevels + 1
+		game_levels = Level.where("game_id = ?", game_id)
+        game_levels.order("levelno")
+        last = game_levels.find(:last)
+		#@lastno = last.levelno + 1
+		self.levelno = last.levelno + 1
 	
 	end
 	
 	def enforce_sequential_levels
-		gamelevels = Level.where("game_id = ?", :game_id)
-		gamelevels.order('levelno')
-		howmanylevels = Level.find(self.game_id).count
+		game_levels = Level.where("game_id = ?", game_id)
+        game_levels.order('levelno ASC, xp_to_next_level ASC')
+		#game_levels.order('xp_to_next_level ASC')
+		howmanylevels = game_levels.count
 		
 		lastlevel = 1
 		if (howmanylevels > 0)
-			gamelevels.each do |level|
+			game_levels.each do |level|
 			thislevel = level
 				if (thislevel.levelno != lastlevel + 1 && thislevel.levelno > 1) 
 					thislevel.levelno = lastlevel + 1
@@ -54,6 +55,8 @@ class Level < ActiveRecord::Base
 			end	
 		
 		end
+		self.levelno = lastlevel
+		
 		
 	end
 	
