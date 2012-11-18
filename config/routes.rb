5976613d02fcec28ff43f12ceea612b0678be6df
Ron Giambalvo <further077@gmail.com>
2012-11-18 00:37:12 -0600

    Increment button functions when pressed
    This is begging for AJAX calls but I just could not get it to work out.
    Right now the onClick will submit the form as well as quickly refresh
    the page. If you delay the clicks by a second, the page refreshes so
    quickly it is not noticeable.
    
    To do: Should we limit the max amount of XP to the sum of all game
    levels xp? Right now user Ron has 48 XP with only 15 xp on level 2.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ LevelBadger::Application.routes.draw do
   match "profile", :to => "session#profile"
   match "settings", :to => "session#setting"
   match "login_attempt", :to =>"session#login_attempt"
-
+  
 
   
   # The priority is based upon order of creation:
